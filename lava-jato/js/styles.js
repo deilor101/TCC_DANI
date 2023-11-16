477390f48ddc45ade424b4004d4da29564580438
Danila <danila.cordeiro@hotmail.com>
2023-11-16 18:32:57 -0300

    opa

--- lava-jato/js/styles.js
+++ lava-jato/js/styles.js
@@ -1,21 +1,6 @@
-// import { createPopper } from '@popperjs/core/lib/popper-lite.js';
-// import preventOverflow from '@popperjs/core/lib/modifiers/preventOverflow.js';
-// import flip from '@popperjs/core/lib/modifiers/flip.js';
-
-// const button = document.querySelector('#button');
-// const tooltip = document.querySelector('#tooltip');
-
-// createPopper(button, tooltip, {
-//   modifiers: [preventOverflow, flip],
-// })
-
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-var exampleEl = document.getElementById('example')
-var tooltip = new bootstrap.Tooltip(exampleEl, options)
-var exampleEl = document.getElementById('example')
-var tooltip = new bootstrap.Tooltip(exampleEl, {
-  boundary: document.body // or document.querySelector('#boundary')
-})
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl, {
+      customClass: 'custom-tooltip' 
+    })
+  });
