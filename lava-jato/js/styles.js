75fb6bd9fe187c025b9331f893eadfb7c31094a8
Danila <danila.cordeiro@hotmail.com>
2023-11-22 22:39:48 -0300

    Adição do search

--- lava-jato/js/styles.js
+++ lava-jato/js/styles.js
@@ -4,3 +4,33 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
       customClass: 'custom-tooltip' 
     })
   });
+
+  const content = document.querySelector(".content");
+  const inputSearch = document.querySelector("input[type='search']");
+  
+  let items = [];
+  
+  inputSearch.oninput = () => {
+    content.innerHTML = "";
+  
+    items
+      .filter((item) =>
+        item.toLowerCase().includes(inputSearch.value.toLowerCase())
+      )
+      .forEach((item) => addHTML(item));
+  };
+  
+  function addHTML(item) {
+    const div = document.createElement("div");
+    div.innerHTML = item;
+    content.append(div);
+  }
+  
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((data) => data.json())
+    .then((users) => {
+      users.forEach((user) => {
+        addHTML(user.name);
+        items.push(user.name);
+      });
+    });
